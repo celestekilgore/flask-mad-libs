058bd5731b3020f78cf0c4a72bdab3f5f5a15461
Jessica Biondi <jbiondi.cs@gmail.com>
2023-09-27 19:22:12 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+from flask_debugtoolbar import DebugToolbarExtension
+
+from stories import silly_story
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
+
+debug = DebugToolbarExtension(app)
+
+@app.get('/questions')
+def display_form():
+    """ docstring """
+
+    prompts = silly_story.prompts
+
+    return render_template('questions.html', prompts=prompts)
+
+
+
